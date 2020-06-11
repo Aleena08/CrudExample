704b8e579c23c58e2657f233146b042263cb2dd4
Aleena Sunny <aleenasunny6@gmail.com>
2020-06-11 21:34:34 +0530

    Initial commit

---
+++ src/main/java/com/example/demo/CrudEg1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudEg1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudEg1Application.class, args);
+	}
+
+}
